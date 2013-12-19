9a6acf891d66aaaa542507930132c66baa12e29a
adrian <adrian-sw@aporter.org>
2013-12-19 16:52:04 -0500

    minor:  make the script runnable from anywhere

--- stack/start-bitcoin-abe.sh
+++ stack/start-bitcoin-abe.sh
@@ -5,9 +5,9 @@ set -e
 renice +20 -p $$ >/dev/null
 ionice -n7 -p $$
 
-cp -a abe.conf bitcoin-abe/
+cp -a ~/abe.conf ~/bitcoin-abe/
 
-cd bitcoin-abe-color-explorer/
+cd ~/bitcoin-abe-color-explorer/
 
 #strace -ostrace.txt -s200
 python -m Abe.abe --config abe.conf >bitcoin-abe-log.txt 2>&1 &

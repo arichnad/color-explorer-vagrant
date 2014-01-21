0c4d4880f70a000103b0658eacf919344f0efc5a
adrian <adrian-sw@aporter.org>
2014-01-21 16:27:00 -0500

    coins for testing.  to be used later.

--- enable-system-tests.sh
+++ enable-system-tests.sh
@@ -10,5 +10,8 @@ cat >"$FILE" <<END
 #use "yes" or "no"
 SYSTEM_TESTS_ENABLED=yes
 
+#public address:  mqtQH8jxv9LoSo676MsfNqr81FCLbWfjph
+TESTNET_PRIVATE_KEY=92b6RYqA2GQXhMCg8YLR73AD2tHFu8ZdnxUjUB3yGn8Tj8iqjKE
+
 END
 
